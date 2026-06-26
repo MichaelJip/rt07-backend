3dd38f2c07d7bd8081cce1973fe34553587c9ac3
Michael <jipmichael1@gmail.com>
2026-06-26 20:17:22 +0700

    chore: Database check

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
 import { DATABASE_URL } from "./env";
 
+let isConnected = false;
+
 const connect = async () => {
+  if (isConnected) return "Database already connected!";
+
   try {
-    await mongoose.connect(DATABASE_URL);
-    return Promise.resolve("Database connected!");
+    await mongoose.connect(DATABASE_URL, {
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 30000,
+    });
+    isConnected = true;
+    return "Database connected!";
   } catch (error) {
     return Promise.reject(error);
   }
